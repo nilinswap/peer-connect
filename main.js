11631c1bc1014a26021557e87d7053e0c0090aa0
Swapnil Sharma <nilinswap@gmail.com>
2021-04-13 20:41:53 +0530

    for display

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ let peer = new Peer({
 
 });
 
-console.log("deployed annie frank");
+console.log("deployed annie");
 
 peer.on('open', function(id) {    // on start
     console.log('My peer ID is: ' + id);
