f073802ff830b0ebe3a21c47d7062c8d0db9805a
Swapnil Sharma <nilinswap@gmail.com>
2021-04-22 17:52:41 +0530

    ready to test

---
+++ hatchtml.js
@@ -0,0 +1,57 @@
+
+var chat_popup_html_string = `
+    <div class="container">
+
+
+<!--        for chat popup-->
+        <button class="open-button" id="open-chat-button" >Chat</button>
+
+        <div class="chat-popup" id="myForm">
+            <p> Your Peer Id is - <span id="peer-id"></span></p>
+            <br>
+            <form id="remote-peer-form" action="" method="GET">
+                <label for="remote-peer-id">Enter a Peer Id to connect with - </label>
+                <input name="remote-peer-id" id="remote-peer-id">
+                <input type="submit">
+            </form>
+
+            <div id="notice-board">
+                <p>Status is - <span id="connection-status"> unconnected. </span></p>
+                <button id="disconnect-button" style="display: none;">Disconnect</button>
+            </div>
+
+            <div id="audio-div">
+                <audio id="audio-element" autoplay controls ></audio>
+                <button id="call-button">call</button>
+                <button id="hangup-button">hang up</button>
+            </div>
+
+
+
+            <div class="messaging">
+                <div class="inbox_msg">
+
+                    <div class="mesgs" id="mesgs-scrollable">
+                        <div class="msg_history">
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <form id="message-form" action="" method="POST">
+                    <input name="message-content" id="message-content">
+                    <input type="submit" value="send">
+                </form>
+
+            </div>
+            <button type="button" class="btn cancel"  id="close-chat-button" >Close</button>
+        </div>
+        <div >
+            created by nilinswap
+        </div>
+    </div>
+`;
+
+export default chat_popup_html_string;
+
--- utils.js
+++ utils.js
@@ -3,3 +3,18 @@ let element = document.getElementById("mesgs-scrollable");
 export function updateScroll(){
     element.scrollTop = element.scrollHeight;
 }
+
+export function htmlToElement(html) {
+    let template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+
+export function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+export function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
